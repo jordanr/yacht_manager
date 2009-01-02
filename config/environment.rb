ed59385ae6f6cd0eb66bde050a51fad2272ed67e
Richard Jordan <jordanr@cs.washington.edu>
2009-01-02 19:52:12 +0000

    adding gem dependencies

--- config/environment.rb
+++ config/environment.rb
@@ -26,6 +26,10 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "aws-s3", :lib => "aws/s3"
 
+  config.gem "attachment_fu"
+  config.gem "restful_authentication"
+  config.gem "yacht_manager"
+
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
