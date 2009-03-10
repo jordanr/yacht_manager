dec7314704ee54a7d84829b34e0a595e7f74e98a
root <richard@jordanyachts.com>
2009-03-10 10:59:15 -0400

    new schema

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090305162818) do
+ActiveRecord::Schema.define(:version => 20090309225642) do
 
   create_table "details", :force => true do |t|
     t.string   "title"
@@ -33,6 +33,7 @@ ActiveRecord::Schema.define(:version => 20090305162818) do
     t.string   "yacht_specification_designer"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "active"
   end
 
   create_table "photos", :force => true do |t|
