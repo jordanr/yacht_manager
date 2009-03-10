b1a7253549efac16abbc89fd183adf600a3cb43d
Richard <richard@jordanyachts.com>
2009-03-10 11:23:52 -0400

    add fix to test allow_nil validation

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,6 +2,6 @@ class Listing < ActiveRecord::Base
   has_many :details
   has_many :photos
 
-  validates_numericality_of :price, :yacht_specification_length
-  validates_inclusion_of :yacht_specification_year, :in => 1800..2100
+  validates_numericality_of :price, :yacht_specification_length, :allow_nil=>true
+  validates_inclusion_of :yacht_specification_year, :in => 1800..2100, :allow_nil=>true
 end
