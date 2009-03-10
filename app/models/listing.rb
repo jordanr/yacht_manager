32ea7d4572010bc7422827f3c07d5020c6d06f31
Richard <richard@jordanyachts.com>
2009-03-10 10:54:40 -0400

    mergin new release

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,7 @@
 class Listing < ActiveRecord::Base
   has_many :details
   has_many :photos
+
+  validates_numericality_of :price, :yacht_specification_length
+  validates_inclusion_of :yacht_specification_year, :in => 1800..2100
 end
