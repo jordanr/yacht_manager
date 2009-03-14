8fbf8931798cddc7074902faea5c1a9288d32b3c
Richard <richard@jordanyachts.com>
2009-03-13 21:39:57 -0400

    adding transfers

---
+++ db/migrate/20090314013640_create_transfers.rb
@@ -0,0 +1,15 @@
+class CreateTransfers < ActiveRecord::Migration
+  def self.up
+    create_table :transfers do |t|
+      t.integer :listing_id
+      t.integer :account_id
+      t.integer :remote_id
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :transfers
+  end
+end
